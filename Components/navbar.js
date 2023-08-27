55ae8cb985037c66ef3d85bc6510c07c0496ba22
Saransh <saini.saransh03@gmail.com>
2023-08-27 11:35:47 +0530

    Working Typewriter

---
+++ Components/navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+import styler from "../styles/Navbar.module.css"
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <>
+    
+        <div className={styler.container}>
+          <div className={styler.profile}>
+            <Image src="/logo.jpg" alt="Logo" width={233} height={233} className={styler.logo}></Image>
+            <span className={styler.name}>Data Ki Science</span>
+            </div>
+            <div className={styler.link_box}>
+              <Link href="/" className={styler.link}>Home</Link>
+              <Link href="/about" className={styler.link}>About Me</Link>
+              <Link href="/learn" className={styler.link}>Learn</Link>
+              <Link href="/blog" className={styler.link}>Blogs</Link>
+              <Link href="/projects" className={styler.link}>My Projects</Link>
+              <Link href="/contact" className={styler.link}>Contact Me</Link>
+            </div>
+        </div>
+        <hr className={styler.hr}></hr>
+    </>
+  )
+}
+
+export default Navbar
