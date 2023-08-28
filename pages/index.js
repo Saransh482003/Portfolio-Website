87357bfd000a82d514f7bd60f4feba5d8bba45b4
Saransh <saini.saransh03@gmail.com>
2023-08-29 00:15:33 +0530

    Index Finally Done

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,16 @@ import Navbar from '@/Components/navbar'
 import styler from "../styles/Index.module.css"
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
+import { resolve } from 'styled-jsx/css'
 
 const inter = Inter({ subsets: ['latin'] })
-const skills = ["Python", "Java", "JavaScript", "Machine Learning", "Deep Learning", "DSA", "Android App Development", "Full Stack Development"]
+const skills = ['Python', 'Java', 'JavaScript', 'MS Excel', 'MySQL', 'Data Preprocessing', 'DSA', 'Machine Learning', 'Deep Learning', 'Android App Development', 'Full Stack Development']
 const maxString = Math.max(...skills.map(x => x.length))
+// fetch("http://localhost:3000/api/skills").then((a) => {
+//   return a.json()
+// }).then((res) => {
+//   console.log(Object.values(JSON.parse(res)),23)
+// })
 
 export default function Home() {
   const [index, setIndex] = useState(0);
@@ -17,13 +23,28 @@ export default function Home() {
   const [ind, setInd] = useState(0);
   const [char, setChar] = useState("");
   const [flag, setFlag] = useState(false)
+  const [explore, setExplore] = useState([])
 
+  useEffect(() => {
+    fetch("http://localhost:3000/api/explorer").then((a) => {
+      return a.json()
+    }).then((res) => {
+      setExplore(JSON.parse(res))
+    })
+  }, [])
+
+  // useEffect(()=>{
+  //   setSkill(skills[0])
+  //   setMaxString(Math.max(...skills.map(x => x.length)))
+  //   return ()=>{}
+  // },[skills])
   useEffect(() => {
     const skillIter = setInterval(() => {
       setIndex((index + 1) % skills.length)
     }, 75 * (skill.length + maxString) + 5)
     return () => clearInterval(skillIter)
-  }, [index, skill])
+  }, [skill, index, skills, maxString])
+
   useEffect(() => {
     const charCounter = setInterval(() => {
       if (ind < skill.length) {
@@ -34,7 +55,8 @@ export default function Home() {
     return () => {
       clearInterval(charCounter)
     }
-  }, [skill, ind])
+  }, [skill, ind, skills])
+
   useEffect(() => {
     if ((char.length == skill.length) && flag == false) {
       // console.log("hgv")
@@ -42,6 +64,7 @@ export default function Home() {
       setFlag(true)
     }
   })
+
   useEffect(() => {
     const decChar = setInterval(() => {
       if (flag) {
@@ -52,12 +75,15 @@ export default function Home() {
       clearInterval(decChar)
     }
   }, [char, skill])
+
   useEffect(() => {
     console.log(char)
     let a = document.getElementById("skiller")
     a.innerHTML = char
   }, [skill, char, ind])
+
   useEffect(() => {
+    // console.log(skill,maxString)
     setSkill(skills[index])
     setInd(0)
     setChar("")
@@ -75,11 +101,11 @@ export default function Home() {
             <h1 className={styler.iknow}>I know : </h1>
             <h1 className={styler.skills} id='skiller'>Python</h1>
           </div>
-          <p className={styler.intro}>Greetings! I'm a dedicated student passionate about Data Science, pursuing a BS in Data Science at IIT Madras and a B.Sc. in Data Analytics. I share insights on my YouTube channel, guide learners as a Section Leader at Stanford's "Code in Place," and organize coding events. Projects like "MaanSick" Mental Health App and "Anukaran" for auditory impairments reflect my tech-driven compassion. Currently scratch-coding a comprehensive Data Structure and Algorithm library to enhance my skills.</p>
+          <p className={styler.intro}>Greetings! I'm a dedicated student passionate about Data Science, pursuing a BS in Data Science at IIT Madras and a B.Sc. in Data Analytics at DSEU. I share insights on my YouTube channel, guide learners as a Section Leader at Stanford University's "Code in Place," and organize coding events. Projects like "MaanSick" Mental Health App and "Anukaran" for auditory impairments reflect my tech-driven compassion. Currently scratch-coding a comprehensive Data Structure and Algorithm library to enhance my skills.</p>
           <p className={styler.wel}>Welcome to my journey of learning, leadership, and innovation!!!</p>
         </div>
         <div className={styler.pic}>
-          <Image src="/mypic.png" width={800} height={898} className={styler.mypic}></Image>
+          <Image src="/mypic.png" width={800} height={898} alt={"mypic"} className={styler.mypic}></Image>
         </div>
       </section>
       {/* <hr></hr> */}
@@ -87,7 +113,21 @@ export default function Home() {
         <h1 className={styler.guide}>Exploration Guide</h1>
         <div className={styler.guide_body}>
 
-          <div className={styler.card}>
+          {
+            explore.map((exp, index) => {
+              return <div className={styler.card}>
+                <Image src={exp.img} width={1076} height={917} alt={exp.title} className={styler.cardImg}></Image>
+                <div className={styler.cardInfo}>
+                  <p className={styler.infoHead}>{exp.title}</p>
+                  <p className={styler.infoDesc}>{exp.description}</p>
+                </div>
+                <Link href={exp.link} className={styler.goBtn}>
+                  <p className={styler.btn}>Explore</p>
+                </Link>
+              </div>
+            })
+          }
+          {/* <div className={styler.card}>
             <Image src="/Home Cards/about.jpg" width={1076} height={917} className={styler.cardImg}></Image>
             <div className={styler.cardInfo}>
               <p className={styler.infoHead}>ABOUT ME</p>
@@ -129,7 +169,7 @@ export default function Home() {
             <Link href="/projects" className={styler.goBtn}>
               <p className={styler.btn}>Explore</p>
             </Link>
-          </div>
+          </div> */}
         </div>
       </section>
 
