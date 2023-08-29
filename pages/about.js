f3adcbe20920b98f1238160f4556f9f7b75e77ce
Saransh <saini.saransh03@gmail.com>
2023-08-29 15:48:44 +0530

    About Page Start

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+import styler from "../styles/About.module.css"
 
 const about = () => {
+  const [education, setEducation] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3000/api/education").then((a) => {
+      return a.json()
+    }).then((res) => {
+      setEducation(JSON.parse(res))
+    })
+  })
   return (
-    <div>about</div>
+    <>
+      <section className={styler.about}>
+        <p className={styler.head}>ABOUT ME</p>
+        <p className={styler.quote}>"Like a quill poised above an open page, my 'About Me' is a journey of discovery, where each word penned and every experience lived adds to the ever-growing narrative of who I am and who I aim to be."</p>
+      </section>
+      <section className={styler.edu}>
+        <h1 className={styler.eduHead}>Educational Background</h1>
+        {
+          education.map((edu, ind) => {
+            return <div className={styler.eduCard}>
+              <div className={styler.eduInfo}>
+                <p className={styler.eduCardHead}>{edu.program}</p>
+                <p className={styler.eduCardName}>{edu.name}</p>
+                <p className={styler.eduCardName}>{edu.duration} • {edu.years}</p>
+                <p className={styler.eduCardDesc}>{edu.description}</p>
+              </div>
+              <div className={styler.eduImg}>
+                <Image src={edu.img} height={500} width={500} alt={edu.name} className={styler.eduLogo}></Image>
+              </div>
+            </div>
+          })
+        }
+
+        <div className={styler.eduCard}>
+          <div className={styler.eduInfo}>
+            <p className={styler.eduCardHead}>B.Sc. in Data Analytics</p>
+            <p className={styler.eduCardName}>Delhi Skill and Enterpreneurship University</p>
+            <p className={styler.eduCardName}>2021 - Present • 3 Years</p>
+            <p className={styler.eduCardDesc}>
+              Currently, I am immersed in a transformative educational journey at the prestigious IIT Madras, pursuing a comprehensive Bachelor of Science in Programming and Data Science. This four-year program is equipping me with a profound understanding of programming intricacies, data analysis, and scientific methodologies. As I progress through its dynamic curriculum, which spans foundational, diploma, and degree levels, I am engaged in interactive online live classes, immersive projects, and credit-based courses. This experience is molding me into a skilled professional. My commitment to excellence and my passion for learning are driving me to embrace every facet of this educational odyssey with enthusiasm and dedication.</p>
+          </div>
+          <div className={styler.eduImg}>
+            <Image src="/About/Educational Background/IITM.png" height={500} width={500} alt="IITM" className={styler.eduLogo}></Image>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,6 @@ import { resolve } from 'styled-jsx/css'
 const inter = Inter({ subsets: ['latin'] })
 const skills = ['Python', 'Java', 'JavaScript', 'MS Excel', 'MySQL', 'Data Preprocessing', 'DSA', 'Machine Learning', 'Deep Learning', 'Android App Development', 'Full Stack Development']
 const maxString = Math.max(...skills.map(x => x.length))
-// fetch("http://localhost:3000/api/skills").then((a) => {
-//   return a.json()
-// }).then((res) => {
-//   console.log(Object.values(JSON.parse(res)),23)
-// })
 
 export default function Home() {
   const [index, setIndex] = useState(0);
@@ -25,6 +20,7 @@ export default function Home() {
   const [flag, setFlag] = useState(false)
   const [explore, setExplore] = useState([])
 
+
   useEffect(() => {
     fetch("http://localhost:3000/api/explorer").then((a) => {
       return a.json()
@@ -33,11 +29,6 @@ export default function Home() {
     })
   }, [])
 
-  // useEffect(()=>{
-  //   setSkill(skills[0])
-  //   setMaxString(Math.max(...skills.map(x => x.length)))
-  //   return ()=>{}
-  // },[skills])
   useEffect(() => {
     const skillIter = setInterval(() => {
       setIndex((index + 1) % skills.length)
@@ -77,7 +68,7 @@ export default function Home() {
   }, [char, skill])
 
   useEffect(() => {
-    console.log(char)
+    // console.log(char)
     let a = document.getElementById("skiller")
     a.innerHTML = char
   }, [skill, char, ind])
@@ -105,7 +96,7 @@ export default function Home() {
           <p className={styler.wel}>Welcome to my journey of learning, leadership, and innovation!!!</p>
         </div>
         <div className={styler.pic}>
-          <Image src="/mypic.png" width={800} height={898} alt={"mypic"} className={styler.mypic}></Image>
+          <Image src="/mypiced.png" width={800} height={898} alt={"mypic"} className={styler.mypic}></Image>
         </div>
       </section>
       {/* <hr></hr> */}
@@ -127,49 +118,6 @@ export default function Home() {
               </div>
             })
           }
-          {/* <div className={styler.card}>
-            <Image src="/Home Cards/about.jpg" width={1076} height={917} className={styler.cardImg}></Image>
-            <div className={styler.cardInfo}>
-              <p className={styler.infoHead}>ABOUT ME</p>
-              <p className={styler.infoDesc}>Step into my world of curiosity and exploration. Discover my journey as a devoted Data Science enthusiast, and unravel the layers of my educational pursuits, leadership experiences, and innovative projects!</p>
-            </div>
-            <Link href="/about" className={styler.goBtn}>
-              <p className={styler.btn}>Explore</p>
-            </Link>
-          </div>
-
-          <div className={styler.card}>
-            <Image src="/Home Cards/learn.jpg" width={1000} height={667} className={styler.cardImg}></Image>
-            <div className={styler.cardInfo}>
-              <p className={styler.infoHead}>LEARN</p>
-              <p className={styler.infoDesc}>Curious about Data Science and Machine Learning? Tune in to "Data Ki Science" for engaging insights and tutorials. Learn Data Science concepts like Machine Learning, Python Programming, and a lot more. Let's explore together!</p>
-            </div>
-            <Link href="/learn" className={styler.goBtn}>
-              <p className={styler.btn}>Explore</p>
-            </Link>
-          </div>
-
-          <div className={styler.card}>
-            <Image src="/Home Cards/blog.jpg" width={1000} height={667} className={styler.cardImg}></Image>
-            <div className={styler.cardInfo}>
-              <p className={styler.infoHead}>BLOGS</p>
-              <p className={styler.infoDesc}>Step into a treasure trove of insights at my blog corner. Uncover articles on Data Science, Machine Learning, and more. Let's explore the fascinating world of tech and knowledge together!</p>
-            </div>
-            <Link href="/blogs" className={styler.goBtn}>
-              <p className={styler.btn}>Explore</p>
-            </Link>
-          </div>
-
-          <div className={styler.card}>
-            <Image src="/Home Cards/projects.jpg" width={1920} height={1080} className={styler.cardImg}></Image>
-            <div className={styler.cardInfo}>
-              <p className={styler.infoHead}>MY PROJECTS</p>
-              <p className={styler.infoDesc}>Discover a fusion of creativity and innovation in my project showcase. From Machine Learning marvels to captivating Web and Android developments, explore the exciting landscapes of my work. Let's journey through the realm of tech and possibilities!</p>
-            </div>
-            <Link href="/projects" className={styler.goBtn}>
-              <p className={styler.btn}>Explore</p>
-            </Link>
-          </div> */}
         </div>
       </section>
 
