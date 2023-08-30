75c9e639d2bfcf9e9c6b3c40316313caddc09029
Saransh <saini.saransh03@gmail.com>
2023-08-30 15:30:22 +0530

    Extra Curricular Added

--- pages/about.js
+++ pages/about.js
@@ -4,16 +4,29 @@ import styler from "../styles/About.module.css"
 
 const about = () => {
   const [education, setEducation] = useState([])
+  const [extra, setExtra] = useState([])
   useEffect(() => {
     fetch("http://localhost:3000/api/education").then((a) => {
       return a.json()
     }).then((res) => {
       setEducation(JSON.parse(res))
     })
-  })
-  useEffect(()=>{
+  },[])
+  useEffect(() => {
+    fetch("http://localhost:3000/api/extra").then((a) => {
+      return a.json()
+    }).then((res) => {
+      setExtra(JSON.parse(res))
+    })
+  },[])
+  useEffect(() => {
     let a = document.getElementById("sec")
-    a.style.height = ((230+20)*education.length+100)+"px"
+    a.style.height = ((230 + 20) * education.length + 100) + "px"
+  })
+  useEffect(() => {
+    let a = document.getElementById("extraCon")
+    console.log()
+    a.style.height = (40*Math.ceil((education.length-1)/3)) + "rem";
   })
   return (
     <>
@@ -27,7 +40,7 @@ const about = () => {
           education.map((edu, ind) => {
             return <div className={styler.eduCard}>
               <div className={styler.eduInfo}>
-                <p className={styler.eduCardHead}>{edu.program}</p>
+                <h1 className={styler.eduCardHead}>{edu.program}</h1>
                 <p className={styler.eduCardName}>{edu.name}</p>
                 <p className={styler.eduCardName}>{edu.duration} • {edu.years}</p>
                 <p className={styler.eduCardDesc}>{edu.description}</p>
@@ -38,19 +51,44 @@ const about = () => {
             </div>
           })
         }
+      </section>
+      <section className={styler.extra}>
+        <h1 className={styler.eduHead}>Extra Curricular Activities</h1>
+        <div className={styler.extraContainer} id="extraCon">
+          {extra.map((ex,ind)=>{
+            return <div className={styler.extraCard}>
+            <div className={styler.extraImg}>
+              <Image src={ex.img} width={920} height={404} alt={ex.head} className={styler.extraImger}></Image>
+            </div>
+            <div className={styler.extraInfo}>
+              <h1 className={styler.extraHead}>{ex.head}</h1>
+              <p className={styler.extraDesc}>{ex.description}</p>
+            </div>
+          </div>
+          })
 
-        {/* <div className={styler.eduCard}>
-          <div className={styler.eduInfo}>
-            <p className={styler.eduCardHead}>B.Sc. in Data Analytics</p>
-            <p className={styler.eduCardName}>Delhi Skill and Enterpreneurship University</p>
-            <p className={styler.eduCardName}>2021 - Present • 3 Years</p>
-            <p className={styler.eduCardDesc}>
-              Currently, I am immersed in a transformative educational journey at the prestigious IIT Madras, pursuing a comprehensive Bachelor of Science in Programming and Data Science. This four-year program is equipping me with a profound understanding of programming intricacies, data analysis, and scientific methodologies. As I progress through its dynamic curriculum, which spans foundational, diploma, and degree levels, I am engaged in interactive online live classes, immersive projects, and credit-based courses. This experience is molding me into a skilled professional. My commitment to excellence and my passion for learning are driving me to embrace every facet of this educational odyssey with enthusiasm and dedication.</p>
+          }
+          
+          {/* <div className={styler.extraCard}>
+            <div className={styler.extraImg}>
+              <Image src="/About/Extra Curricular/Stanford.jpg" width={920} height={404} alt="Data Ki Science" className={styler.extraImger}></Image>
+            </div>
+            <div className={styler.extraInfo}>
+              <h1 className={styler.extraHead}>Code in Place Section Leader</h1>
+              <p className={styler.extraDesc}>I volunteered as a Section Leader in Stanford University’s course named Code in Place. It is a course spanning 6 weeks of learning into programming. I was allotted a section of 16 students, from all over the world, to teach Python programming language.</p>
+            </div>
           </div>
-          <div className={styler.eduImg}>
-            <Image src="/About/Educational Background/IITM.png" height={500} width={500} alt="IITM" className={styler.eduLogo}></Image>
-          </div>
-        </div> */}
+          <div className={styler.extraCard}>
+            <div className={styler.extraImg}>
+              <Image src="/About/Extra Curricular/Code Spardha Banner Edited.png" width={920} height={404} alt="Data Ki Science" className={styler.extraImger}></Image>
+            </div>
+            <div className={styler.extraInfo}>
+              <h1 className={styler.extraHead}>Code Spardha Organiser</h1>
+              <p className={styler.extraDesc}>Lead the organizing team of an Online Coding Competition named "Code Spardha" on 28th May 2022 and it’s second instalment on 7 January 2023. 300+ participants from all over Delhi participated in the competition.</p>
+            </div>
+          </div> */}
+
+        </div>
       </section>
     </>
   )
