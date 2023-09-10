0c0437f48c3e0697a5799ba49dbf9b2ba99b131b
Saransh <saini.saransh03@gmail.com>
2023-09-10 12:38:59 +0530

    Project page complete

---
+++ pages/api/projectsFetcher.js
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import *  as fs from "fs"
+export default async function handler(req, res) {
+  let data;
+  let allProjects = []
+  for (let i of ["Completed","Ongoing","Halted","Future Plans"]){
+    data = fs.promises.readFile(`JSONs/Projects/${i}.json`,"utf-8")
+    data = await data
+    allProjects.push(JSON.parse(data))
+  }
+
+
+  res.status(200).json(allProjects)
+}
--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styler from "../styles/Projects.module.css"
 import Image from 'next/image'
 import Link from 'next/link'
 
 
 const projects = () => {
+  const [projects, setProjects] = useState([])
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const colorPalete = ["green", "rgb(248, 132, 0)", "#990000", "black"]
+  const typePalete = ["Completed", "Ongoing", "Halted", "Future Plans"]
+  useEffect(() => {
+    fetch("http://localhost:3000/api/projectsFetcher").then((a) => {
+      return a.json()
+    }).then((res) => {
+      setProjects(res)
+    })
+  }, [])
+
   return (
     <>
       <section className={styler.banner}>
@@ -12,47 +24,52 @@ const projects = () => {
         <p className={styler.quote}>"In my projects, complete and ongoing, I orchestrate a symphony of passion and relentless effort, crafting a masterpiece that defines me."</p>
       </section>
       <section className={styler.projectSpace}>
-        <div className={styler.project}>
-          <div className={styler.projectStatus}>Completed</div>
-          <div className={styler.projectValue}>
-            <div className={styler.projectInfo}>
-              <div className={styler.ImgContainer}>
-                <Image src="/Projects/the-fitting-of-6-models.png" width={200} height={200} className={styler.Img}></Image>
-              </div>
-              <div className={styler.info}>
-                <h1 className={styler.h1}>The Fitting of 6 Models</h1>
-                <p className={styler.date}>4 August 2023</p>
-                <div className={styler.type}>Indivisual</div>
-              </div>
-            </div>
-            <hr></hr>
-            <div className={styler.content}>
-              <div className={styler.descTray}>
-                <p className={styler.descHead}>DESCRIPTION</p>
-                <p className={styler.desc}>I worked on a Machine Learning project, which I like to call "The Fitting of 6 Models", (a bit poetic). The project deals with the classification of mobile phones in 4 price ranges based on their hardware features.
-                  The 6 Models used are:
-                  1) Logistic Regression
-                  2) KNN Classifier
-                  3) KNN Regressor
-                  4) Support Vector Machines (SVM)
-                  5) Decision Trees
-                  6) Random Forest.
+        {
+          projects.map((typer, index) => {
+            return typer.map(proj => {
+              return <div className={styler.project}>
+                <div className={styler.projectStatus} style={{ "backgroundColor": colorPalete[index] }}>{typePalete[index]}</div>
+                <div className={styler.projectValue} style={{ "borderColor": colorPalete[index] }}>
+                  <div className={styler.projectInfo}>
+                    <div className={styler.ImgContainer}>
+                      <Image src="/Projects/the-fitting-of-6-models.png" width={200} height={200} className={styler.Img}></Image>
+                    </div>
+                    <div className={styler.info}>
+                      <h1 className={styler.h1}>{proj.title}</h1>
+                      <p className={styler.date}>{proj.date}</p>
+                      <div className={styler.type} style={{ "backgroundColor": colorPalete[index] }}>{proj.type}</div>
+                    </div>
+                  </div>
+                  <hr></hr>
+                  <div className={styler.content}>
+                    <div className={styler.descTray}>
+                      <p className={styler.descHead}>DESCRIPTION</p>
+                      <p className={styler.desc}>{proj.description}</p>
+                    </div>
+                    <p className={styler.descHead}>SKILLS USED</p>
+                    <div className={styler.skillTray}>
+                      {
+                        proj.skills.map(skill => {
+                          return <Image src={skill.img} width={200} height={200} alt={skill.name} title={skill.title} className={styler.skill}></Image>
+                        })
+                      }
 
-                  The best accuracy of 96% was achieved by SVM model, followed by 95% of Logistic Regression. Also, hyperparameter tuning was done using Cross Validation technique to achieve the best possible accuracy in every case.</p>
-              </div>
-              <p className={styler.descHead}>SKILLS USED</p>
-              <div className={styler.skillTray}>
-                <Image src="/Learn/skills/python.png" width={200} height={200} alt='Python' className={styler.skill}></Image>
-                <Image src="/Learn/skills/ml.png" width={200} height={200} alt='Python' className={styler.skill}></Image>
-                <Image src="/Learn/skills/data_preprocessing.png" width={200} height={200} alt='Python' className={styler.skill}></Image>
-              </div>
-              <div className={styler.links}>
-                <Link href="https://www.linkedin.com/posts/saranshsaini48_the-fitting-of-6-models-by-saransh-saini-activity-6990623528651755520-oprv?utm_source=share&utm_medium=member_desktop" target="_blank" className={styler.link}>Go to Project</Link>
-              </div>
+                    </div>
+                    <div className={styler.links}>
+                      <Link href="https://www.linkedin.com/posts/saranshsaini48_the-fitting-of-6-models-by-saransh-saini-activity-6990623528651755520-oprv?utm_source=share&utm_medium=member_desktop" target="_blank" className={styler.link} style={{
+                        backgroundColor: hoveredIndex === index ? colorPalete[index] : '', borderColor: colorPalete[index], color: hoveredIndex === index ? "" : colorPalete[index]
+                      }} onMouseEnter={() => setHoveredIndex(index)}
+                        onMouseLeave={() => setHoveredIndex(null)}>Go to Project</Link>
+                    </div>
 
-            </div>
-          </div>
-        </div>
+                  </div>
+                </div>
+              </div>
+            })
+
+          })
+        }
+
       </section>
     </>
   )
