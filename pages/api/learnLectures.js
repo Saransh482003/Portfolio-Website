a95748c0a280ce992c6e13203e279bc42d99e8d3
Saransh <saini.saransh03@gmail.com>
2023-09-12 21:55:58 +0530

    Testing 4

--- pages/api/learnLectures.js
+++ pages/api/learnLectures.js
@@ -2,10 +2,11 @@
 import *  as fs from "fs"
 export default async function handler(req, res) {
   let data;
+  console.log("grs")
   data = fs.promises.readFile("JSONs/Learn/lecture.json","utf-8")
   data = await data
   console.log(data)
-  console.log("grs")
+
   res.status(200).json(JSON.parse(data))
   // res.status(200).json({"ret":12})
 }
