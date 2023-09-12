8116e7b1e56382a621715585162c9c496d300c1e
Saransh <saini.saransh03@gmail.com>
2023-09-12 22:06:13 +0530

    Testing 5

--- pages/api/learnLectures.js
+++ pages/api/learnLectures.js
@@ -1,9 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import *  as fs from "fs"
-export default async function handler(req, res) {
-  let data;
-  data = fs.promises.readFile("public/JSONs/Learn/lecture.json","utf-8")
-  data = await data
+import fs from "fs/promises";
+import path from "path";
 
-  res.status(200).json(JSON.parse(data))
+export default async function handler(req, res) {
+  try {
+    // Construct an absolute path to your JSON file
+    const filePath = path.join(process.cwd(), "public", "JSONs", "Learn", "lecture.json");
+
+    // Read the JSON file
+    const data = await fs.readFile(filePath, "utf-8");
+
+    // Parse and send the JSON data as a response
+    res.status(200).json(JSON.parse(data));
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
+
+
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import *  as fs from "fs"
+// export default async function handler(req, res) {
+//   let data;
+//   data = fs.promises.readFile("public/JSONs/Learn/lecture.json","utf-8")
+//   data = await data
+
+//   res.status(200).json(JSON.parse(data))
+// }
