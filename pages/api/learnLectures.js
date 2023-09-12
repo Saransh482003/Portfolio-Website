307b8c5a4607080d07e48525c038b993617a6f23
Saransh <saini.saransh03@gmail.com>
2023-09-12 21:55:07 +0530

    Testing 4

--- pages/api/learnLectures.js
+++ pages/api/learnLectures.js
@@ -1,9 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import *  as fs from "fs"
 export default async function handler(req, res) {
-  // let data;
-  // data = fs.promises.readFile("JSONs/Learn/lecture.json","utf-8")
-  // data = await data
-  // res.status(200).json(JSON.parse(data))
-  res.status(200).json({"ret":12})
+  let data;
+  data = fs.promises.readFile("JSONs/Learn/lecture.json","utf-8")
+  data = await data
+  console.log(data)
+  console.log("grs")
+  res.status(200).json(JSON.parse(data))
+  // res.status(200).json({"ret":12})
 }
