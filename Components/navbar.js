b7022c5017d39f7aed6503c89fbe523ef3096725
Saransh <saini.saransh03@gmail.com>
2023-09-14 20:36:36 +0530

    Responsive 20%

--- Components/navbar.js
+++ Components/navbar.js
@@ -8,19 +8,19 @@ const Navbar = () => {
     <>
 
       <div className={styler.container}>
-        <Link href="/">
-          <div className={styler.profile}>
+        <Link href="/" className={styler.profile}>
             <Image src="/logo.jpg" alt="Logo" width={233} height={233} className={styler.logo}></Image>
             <span className={styler.name}>Data Ki Science</span>
-          </div>
         </Link>
-        <div className={styler.link_box}>
+        <div className={styler.restCase}>
+          <div className={styler.link_box}>
           <Link href="/" className={styler.link}>Home</Link>
           <Link href="/about" className={styler.link}>About Me</Link>
           <Link href="/learn" className={styler.link}>Learn</Link>
           <Link href="/blogs" className={styler.link}>Blogs</Link>
           <Link href="/projects" className={styler.link}>My Projects</Link>
           <Link href="/contact" className={styler.link}>Contact Me</Link>
+        </div>
         </div>
       </div>
       <hr className={styler.hr}></hr>
