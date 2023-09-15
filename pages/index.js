60d471a802e2a6539a798262648566d7ceae3b20
Saransh <saini.saransh03@gmail.com>
2023-09-15 16:51:27 +0530

    Responsiveness done

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Link from 'next/link'
 import { resolve } from 'styled-jsx/css'
 
 const inter = Inter({ subsets: ['latin'] })
-const skills = ['Python', 'Java', 'JavaScript', 'MS Excel', 'MySQL', 'Data Preprocessing', 'DSA', 'Machine Learning', 'Deep Learning', 'Android App Development', 'Full Stack Development',"ReactJS","NextJS"]
+const skills = ['Python', 'Java', 'JavaScript', 'MS Excel', 'MySQL', 'Data Preprocessing', 'DSA', 'Machine Learning', 'Deep Learning', 'Android App Development', 'Full Stack Development', "ReactJS", "NextJS"]
 const maxString = Math.max(...skills.map(x => x.length))
 
 export default function Home() {
@@ -84,6 +84,15 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/icon.png" />
+        <title>Data Ki Science</title>
+        <meta name="description" content="Hi! I am Saransh Saini., a dedicated student passionate about Data Science, pursuing a BS in Data Science at IIT Madras and a B.Sc. in Data Analytics at DSEU. Welcome to my portfolio website where you can explore about me in great detail. "></meta>
+        <meta name='keywords' content='saransh saini, data ki science, saransh, portfolio website, web developer, nextjs, reactjs'></meta>
+      </Head>
       <section className={styler.section}>
         <div className={styler.welcome}>
           <h1 className={styler.iam}>Hi! I am </h1>
