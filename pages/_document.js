1a33fc81ce9b0f140ca91a3a47b8135eb8cceb75
Saransh <saini.saransh03@gmail.com>
2023-09-15 23:28:40 +0530

    Responsiveness completely done

--- pages/_document.js
+++ pages/_document.js
@@ -3,15 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/icon.png" />
-        <title>Data Ki Science</title>
-        <meta name="description" content="Hi! I am Saransh Saini., a dedicated student passionate about Data Science, pursuing a BS in Data Science at IIT Madras and a B.Sc. in Data Analytics at DSEU. Welcome to my portfolio website where you can explore about me in great detail. "></meta>
-        <meta name='keywords' content='saransh saini, data ki science, saransh, portfolio website, web developer, nextjs, reactjs'></meta>
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -84,15 +84,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/icon.png" />
-        <title>Data Ki Science</title>
-        <meta name="description" content="Hi! I am Saransh Saini., a dedicated student passionate about Data Science, pursuing a BS in Data Science at IIT Madras and a B.Sc. in Data Analytics at DSEU. Welcome to my portfolio website where you can explore about me in great detail. "></meta>
-        <meta name='keywords' content='saransh saini, data ki science, saransh, portfolio website, web developer, nextjs, reactjs'></meta>
-      </Head>
       <section className={styler.section}>
         <div className={styler.welcome}>
           <h1 className={styler.iam}>Hi! I am </h1>
