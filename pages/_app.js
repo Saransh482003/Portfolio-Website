9f61eb9675ec10e472ac4b1936f65cc5631278a5
Saransh <saini.saransh03@gmail.com>
2024-03-03 12:14:31 +0530

    Microsoft Clarify Code Added

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Footer from '@/Components/footer'
 import Navbar from '@/Components/navbar'
 import '@/styles/globals.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
 
@@ -13,9 +14,20 @@ export default function App({ Component, pageProps }) {
       <link rel="icon" href="/icon.png" />
       <title>Data Ki Science</title>
       <meta name="description" content="Hi! I am Saransh Saini., a dedicated student passionate about Data Science, pursuing a BS in Data Science at IIT Madras and a B.Sc. in Data Analytics at DSEU. Welcome to my portfolio website where you can explore about me in great detail. "></meta>
+      <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "larkax97v9");
+      `,
+    }}
+  />
     </Head>
     <Navbar />
     <Component {...pageProps} />
-    <Footer/>
+    <Footer />
   </>
 }
