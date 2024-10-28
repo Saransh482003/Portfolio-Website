353b1faaad3dacf2860ed1adbe2ce53fcedfb8e2
Saransh <saini.saransh03@gmail.com>
2024-10-29 01:00:54 +0530

    Transition

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }) {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
       <link rel="icon" href="/icon.png" />
-      <title>Data Ki Science</title>
+      <title>Full Stack Developer & ML Enthusiast | Saransh Saini</title>
       <meta name="description" content="Hi! I am Saransh Saini., a dedicated student passionate about Data Science, pursuing a BS in Data Science at IIT Madras and a B.Sc. in Data Analytics at DSEU. Welcome to my portfolio website where you can explore about me in great detail. "></meta>
       <script
     dangerouslySetInnerHTML={{
